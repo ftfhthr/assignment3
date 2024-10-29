3259866d4c7c270057582e8f5461931358951082
email <toby.deng@student.tdsb.on.ca>
2024-10-29 11:40:11 -0400

    format

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const heronsForm = document.getElementById('herons');
 
 heronsForm.addEventListener("submit", e => {
 	document.getElementById("result").value = herons(
-		heronsForm.elements['side-a'].value, 
-		heronsForm.elements['side-b'].value, 
+		heronsForm.elements['side-a'].value,
+		heronsForm.elements['side-b'].value,
 		heronsForm.elements['side-c'].value
 	)
 });
@@ -39,11 +39,11 @@ polynomialForm.addEventListener("submit", e => {
 });
 
 const herons = (a, b, c) =>
-	(Math.sqrt(4 * Math.pow(a, 2) * Math.pow(b, 2) - 
-	Math.pow(Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2), 2))) / 4;
+	(Math.sqrt(4 * Math.pow(a, 2) * Math.pow(b, 2) -
+		Math.pow(Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2), 2))) / 4;
 
 const ambiguousCase = (angle, a, b) => {
-	const h = b * Math.sin(angle * (Math.PI/180));
+	const h = b * Math.sin(angle * Math.PI / 180);
 	console.log(`${angle} ${a} ${b} ${h}`)
 	if (angle <= 90) {
 		if (a < h) {
@@ -71,8 +71,8 @@ const ambiguousCase = (angle, a, b) => {
 }
 
 const newtons = (g) => {
-	return g - ((6 * Math.pow(g, 4) - 13 * Math.pow(g, 3) - 18 * Math.pow(g, 2) + 7 * g + 6)/
-	(24 * Math.pow(g, 3) - 39 * Math.pow(g, 2) - 36 * g + 7))
+	return g - (6 * Math.pow(g, 4) - 13 * Math.pow(g, 3) - 18 * Math.pow(g, 2) + 7 * g + 6) /
+		(24 * Math.pow(g, 3) - 39 * Math.pow(g, 2) - 36 * g + 7)
 }
 
 const polynomial = (coefficients, exponents, x) => {
