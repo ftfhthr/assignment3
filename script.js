5beadddd161a4555ab1b30b63ff87f28aba728de
aperson <therealt0bzyt@gmail.com>
2024-10-31 22:19:39 -0400

    add checks

--- script.js
+++ script.js
@@ -38,13 +38,17 @@ polynomialForm.addEventListener("submit", e => {
 	document.getElementById("poly-eval").value = result[1];
 });
 
-const herons = (a, b, c) =>
+const herons = (a, b, c) => {
+	if (a == "" || b == "" || c == "" || isNaN(a) || isNaN(b) || isNaN(c)) return "error";
+
 	(Math.sqrt(4 * Math.pow(a, 2) * Math.pow(b, 2) -
-		Math.pow(Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2), 2))) / 4;
+	Math.pow(Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2), 2))) / 4;
+}
 
 const ambiguousCase = (angle, a, b) => {
+	if (angle == "" || a == "" || b == "" || isNaN(angle) || isNaN(a) || isNaN(b)) return "error";
+
 	const h = b * Math.sin(angle * Math.PI / 180);
-	console.log(`${angle} ${a} ${b} ${h}`)
 	if (angle <= 90) {
 		if (a < h) {
 			return "no triangle";
@@ -71,35 +75,49 @@ const ambiguousCase = (angle, a, b) => {
 }
 
 const newtons = (g) => {
+	if (isNaN(g) || g == "") return ["error", ""];
+
 	let approximation = g - (6 * Math.pow(g, 4) - 13 * Math.pow(g, 3) - 18 * Math.pow(g, 2) + 7 * g + 6) /
 	(24 * Math.pow(g, 3) - 39 * Math.pow(g, 2) - 36 * g + 7);
+
 	let root = approximation;
+	
+	// calculate closest root
 	while (root - g != 0) {
 		g = root;
 		root = g - (6 * Math.pow(g, 4) - 13 * Math.pow(g, 3) - 18 * Math.pow(g, 2) + 7 * g + 6) /
 		(24 * Math.pow(g, 3) - 39 * Math.pow(g, 2) - 36 * g + 7);
 	}
-	console.log(`${approximation}, ${root}`);
+
 	return [approximation, root];
 }
 
 const polynomial = (coefficients, exponents, x) => {
 	const coefficientArray = coefficients.split(" ");
 	const exponentArray = exponents.split(" ");
+
+	// check if coefficient and exponent inputs are empty
 	if (coefficientArray.length != exponentArray.length ||
 		coefficients == "" || exponents == "") return ["error", ""];
+
 	let func = "f(x) = ";
 	let eval = `f(${x}) = `;
 	let result = 0;
-	coefficientArray.forEach((coefficient, index) => {
-		if (isNaN(coefficient) || isNaN(exponentArray[index])) return ["error", ""];
-		func += coefficient + "x^" + exponentArray[index];
-		result += Math.pow(coefficient * x, exponentArray[index]);
-		if (index != coefficientArray.length - 1) {
+
+	for (var i = 0; i < coefficientArray.length; i++) {
+		// check if coefficients and exponents are numbers
+		if (isNaN(coefficientArray[i]) || isNaN(exponentArray[i])) return ["error", ""];
+
+		// create function and calculate result
+		func += `${coefficientArray[i]}x^${exponentArray[i]}`;
+		result += Math.pow(coefficientArray[i] * x, exponentArray[i]);
+		if (i != coefficientArray.length - 1) {
 			func += " + ";
 		}
-	})
+	}
 	eval += result;
+
+	// if x value isn't valid, only return function
 	if (isNaN(x) || x == "") return [func, "error"]
 	return [func, eval];
 }
